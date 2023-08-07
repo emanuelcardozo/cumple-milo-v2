29ebbd413560cf70f0cdaad9f4b2dce9b89ff05f
emanuelcardozo <leuname.01@gmail.com>
2023-08-07 14:53:58 -0300

    Fixing transitions

--- index.js
+++ index.js
@@ -29,25 +29,39 @@ function playSound(src) {
 }
 
 window.onload = function() {
-    $('.page').each(function(i,e){
-        $(this).click(function(event){
-          var x = event.pageX;
-          var y = event.pageY;
+  const pages = $('.page')
+  pages.each(function(i,e){
+    $(this).click(function(event){
+      const self = this
+      if(self.id === "start_section") {
+        playSound('/src/audio/smb_bg.mp3')
+      } else {
+        playSound('/src/audio/smb3_enter_level.wav')
+      }
+      var x = event.pageX;
+      var y = event.pageY;
 
-          var nextItem = i + 1;
-          if (nextItem >= $('.page').length){
-            nextItem = 0;
+      var nextItem = i + 1;
+      if (nextItem >= pages.length){
+        nextItem = 1;
+      }
+      
+      $(pages[nextItem]).css('z-index', parseInt($(this).css('z-index')) + 1);
+      $(pages[nextItem]).css('clip-path', 'circle(0% at '+ x +'px '+ y +'px)');
+
+      console.log(self, nextItem)
+      
+      anime({
+        targets: $('.page')[nextItem],
+        update: function(anim) {
+          $(pages[nextItem]).css('clip-path', 'circle('+ (anim.progress*2) +'% at '+ x +'px '+ y +'px)');
+
+          if(anim.progress === 100 && self.id === "start_section") {
+            self.remove()
+            document.querySelectorAll(`.page:nth-child(${nextItem}) .animate__paused`).forEach((el) => el.classList.remove('animate__paused'))
           }
-          
-          $('.page:eq('+ nextItem +')').css('z-index', parseInt($(this).css('z-index')) + 1);
-          $('.page:eq('+ nextItem +')').css('clip-path', 'circle(0% at '+ x +'px '+ y +'px)');
-          
-          anime({
-            targets: $('.page')[nextItem],
-            update: function(anim) {
-              $('.page:eq('+ nextItem +')').css('clip-path', 'circle('+ (anim.progress*2) +'% at '+ x +'px '+ y +'px)');
-            }
-          });
-        });
+        }
       });
+    });
+  });
 }
